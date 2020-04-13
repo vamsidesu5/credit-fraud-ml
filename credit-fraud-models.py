c1e38ee1ec1663a9ef09a36ed1a45cd6f72a417d
vamsidesu5 <vamsidesu5@gmail.com>
2020-04-13 14:58:34 -0400

    svm

--- credit-fraud-models.py
+++ credit-fraud-models.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
-from sklearn.model_selection import validation_curve
+from sklearn.model_selection import validation_curve, GridSearchCV
+from sklearn.svm import SVC 
 
 credit_fraud = pd.read_csv("default_credit_card_clients.csv")
 # credit_fraud = credit_fraud.sample(10000)
@@ -63,22 +64,30 @@ print(train_Y.shape)
 # plt.legend(loc="best")
 # plt.show()
 
-min_sample_param_range = [5,10,15,20,25,30]
-train_scoreNum, test_scoreNum = validation_curve(
-                                RandomForestClassifier(),
-                                X = train_X, y = train_Y.values.ravel(), 
-                                param_name = 'min_samples_split', 
-                                param_range = min_sample_param_range , cv = 3)
-train_scores_mean = np.mean(train_scoreNum, axis=1)
-test_scores_mean = np.mean(test_scoreNum, axis=1)
+# min_sample_param_range = [5,10,15,20,25,30]
+# train_scoreNum, test_scoreNum = validation_curve(
+#                                 RandomForestClassifier(),
+#                                 X = train_X, y = train_Y.values.ravel(), 
+#                                 param_name = 'min_samples_split', 
+#                                 param_range = min_sample_param_range , cv = 3)
+# train_scores_mean = np.mean(train_scoreNum, axis=1)
+# test_scores_mean = np.mean(test_scoreNum, axis=1)
 
-print(train_scores_mean)
-print(test_scores_mean)
+# print(train_scores_mean)
+# print(test_scores_mean)
 
-plt.title("Validation Curve with Random Forests with Bagging Technique")
-plt.xlabel("Min Sample Split")
-plt.ylabel("Accuracy Score")
-plt.plot(min_sample_param_range, train_scores_mean, label='Training Score', color='red',markerfacecolor='red')
-plt.plot(min_sample_param_range, test_scores_mean, label='Testing Data',color='blue',markerfacecolor='blue')
-plt.legend(loc="best")
-plt.show()
+# plt.title("Validation Curve with Random Forests with Bagging Technique")
+# plt.xlabel("Min Sample Split")
+# plt.ylabel("Accuracy Score")
+# plt.plot(min_sample_param_range, train_scores_mean, label='Training Score', color='red',markerfacecolor='red')
+# plt.plot(min_sample_param_range, test_scores_mean, label='Testing Data',color='blue',markerfacecolor='blue')
+# plt.legend(loc="best")
+# plt.show()
+
+# SVM - Grid Search for Best Hyperparameters 
+
+param_grid = {'C': [0.1,1, 10, 100], 'gamma': [1,0.1,0.01,0.001],'kernel': ['rbf', 'poly', 'sigmoid']}
+grid = GridSearchCV(SVC(),param_grid,refit=True,verbose=2)
+grid.fit(train_X,train_Y.values.ravel())
+print(grid.best_estimator_)
+print(grid.cv_results_)
